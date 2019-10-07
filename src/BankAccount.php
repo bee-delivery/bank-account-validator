4760d473ab26a58f5e4aaef392e550b03718b02e
Diná Moura <dina@beedelivery.com.br>
2019-10-07 11:39:23 -0300

    allow checking or not agency div number

--- src/BankAccount.php
+++ src/BankAccount.php
@@ -5,7 +5,8 @@ namespace BeeDelivery\BankAccountValidator;
 class BankAccount
 {
 
-    private static function validator($bankNumber) {
+    private static function validator($bankNumber)
+    {
 
         $validators = array(
             "001" => BancoDoBrasilValidator::class,         // Banco do Brasil
@@ -29,16 +30,17 @@ class BankAccount
         }
     }
 
-    public static function validate($params){
+    public static function validate($params)
+    {
 
         $errors = array();
         $validator = BankAccount::validator($params->bankNumber);
 
         // fill with zeros
-        for($i = strlen($params->accountNumber); $i < $validator::accountNumberLength(); $i++)
+        for ($i = strlen($params->accountNumber); $i < $validator::accountNumberLength(); $i++)
             $params->accountNumber = "0" . $params->accountNumber;
 
-        for($i = strlen($params->agencyNumber); $i < 4; $i++)
+        for ($i = strlen($params->agencyNumber); $i < 4; $i++)
             $params->agencyNumber = "0" . $params->agencyNumber;
         // end fill
 
@@ -50,29 +52,36 @@ class BankAccount
             array_push($errors, (object) array('description' => $validator::agencyNumberMsgError(), 'code' => "INVALID_AGENCY_NUMBER"));
         }
 
-        if (!$validator::agencyCheckNumberIsValid($params->agencyCheckNumber)) {
-            array_push($errors, (object) array('description' => $validator::agencyCheckNumberMsgError(), 'code' => "INVALID_AGENCY_CHECK_NUMBER"));
+        if (!$params->ignoreAgencyCheckNumber) {
+            if (!$validator::agencyCheckNumberIsValid($params->agencyCheckNumber)) {
+                array_push($errors, (object) array('description' => $validator::agencyCheckNumberMsgError(), 'code' => "INVALID_AGENCY_CHECK_NUMBER"));
+            }
         }
 
         if (!$validator::accountNumberIsValid($params->accountNumber)) {
             array_push($errors, (object) array('description' => $validator::accountNumberMsgError(), 'code' => "INVALID_ACCOUNT_NUMBER"));
         }
 
+
         if (!$validator::accountCheckNumberIsValid($params->accountCheckNumber)) {
             array_push($errors, (object) array('description' => "Dígito da conta corrente inválido", 'code' => "INVALID_ACCOUNT_CHECK_NUMBER"));
         }
 
-        if ($validator::agencyNumberIsValid($params->agencyNumber) && $validator::agencyCheckNumberIsValid($params->agencyCheckNumber)) {
-            if(!$validator::agencyCheckNumberMatch($params)) {
-                array_push($errors, (object) array('description' => "Dígito da agência não corresponde ao número da agência preenchido", 'code' => "AGENCY_CHECK_NUMBER_DONT_MATCH"));
+        if (!$params->ignoreAgencyCheckNumber) {
+            if ($validator::agencyNumberIsValid($params->agencyNumber) && $validator::agencyCheckNumberIsValid($params->agencyCheckNumber)) {
+                if (!$validator::agencyCheckNumberMatch($params)) {
+                    array_push($errors, (object) array('description' => "Dígito da agência não corresponde ao número da agência preenchido", 'code' => "AGENCY_CHECK_NUMBER_DONT_MATCH"));
+                }
             }
         }
 
+
         if ($validator::accountNumberIsValid($params->accountNumber) && $validator::accountCheckNumberIsValid($params->accountCheckNumber)) {
-            if(!$validator::accountCheckNumberMatch($params)) {
+            if (!$validator::accountCheckNumberMatch($params)) {
                 array_push($errors, (object) array('description' => "Dígito da conta não corresponde ao número da conta/agência preenchido", 'code' => "ACCOUNT_CHECK_NUMBER_DONT_MATCH"));
             }
         }
+
 
         return array('errors' => $errors, 'params' => $params);
     }
